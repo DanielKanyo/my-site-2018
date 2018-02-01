563c23828b8f666711b09a49bbe0558327488058
DanielKanyo <danielkanyo992@gmail.com>
2018-02-01 07:02:30 +0100

    circular progressbar changes like interval, style

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -9,34 +9,89 @@ class Skills extends Component {
     super(props);
 
     this.state = {
-      currentPercentageIndex: 70,
+      currentPercentageIndex: 0,
     };
   }
 
   render() {
     return (
-      <Grid>
-        <Row className="show-grid">
-          <Col className="progress" xs={4}>
-              <CircularProgressbar
-                className="progressbar-red"
-                percentage={this.state.currentPercentageIndex}
-                initialAnimation={true}
-                strokeWidth={3}
-                background={false}
-                backgroundPadding={20} />
-
-          </Col>
-          <Col className="progress" xs={4}>
-            React
-          </Col>
-          <Col className="progress" xs={4}>
-            Jquery
-          </Col>
-        </Row>
-      </Grid>
+      <div className="Skills">
+        <div className="component-title skills-title">Skills</div>
+        <Grid>
+          <Row className="show-grid">
+            <Col className="progress" sm={4} xs={6}>
+              <div className="circleElement">
+                JavaScript
+                <ChangingProgressbar
+                  percentages={[0, 80]}
+                  strokeWidth={2}
+                />
+              </div>
+            </Col>
+            <Col className="progress" sm={4} xs={6}>
+              <div className="circleElement">
+                React
+                <ChangingProgressbar
+                  percentages={[0, 60]}
+                  strokeWidth={2}
+                />
+              </div>
+            </Col>
+            <Col className="progress" sm={4} xs={6}>
+              <div className="circleElement">
+                Jquery
+                <ChangingProgressbar
+                  percentages={[0, 80]}
+                  strokeWidth={2}
+                />
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
+}
+
+class ChangingProgressbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentPercentageIndex: 0,
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        currentPercentageIndex: (this.state.currentPercentageIndex + 1) % this.props.percentages.length
+      });
+    }, this.props.interval);
+  }
+
+  getStyles() {
+    return this.props.stylesForPercentage ? (
+      this.props.stylesForPercentage(this.getCurrentPercentage())
+    ) : {};
+  }
+
+  getCurrentPercentage() {
+    return this.props.percentages[this.state.currentPercentageIndex];
+  }
+
+  render() {
+    return (
+      <CircularProgressbar
+        {...this.props}
+        percentage={this.getCurrentPercentage()}
+        styles={this.getStyles()}
+      />
+    );
+  }
+}
+ChangingProgressbar.defaultProps = {
+  interval: 3000,
 }
 
 export default Skills;
