a81f90fccd1426c643d0e9c36a515f3e4ef9d38c
DanielKanyo <danielkanyo992@gmail.com>
2018-02-11 20:41:29 +0100

    some fix

--- src/Components/Hobbies/Hobbies.jsx
+++ src/Components/Hobbies/Hobbies.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Carousel } from 'react-bootstrap';
 /** Style */
 import './Hobbies.css';
 
@@ -7,16 +6,9 @@ class Hobbies extends Component {
   render() {
     return (
       <div className="Hobbies" name="hobbies">
-        <Carousel className="my-carousel">
-          <Carousel.Item>
-            <div className="hobbies-content">
-              <div className="component-title hobbies-title">Developing</div>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
-            second
-          </Carousel.Item>
-        </Carousel>
+        <div className="hobbies-content">
+          <div className="component-title hobbies-title">Hobbies</div>
+        </div>
       </div>
     );
   }
