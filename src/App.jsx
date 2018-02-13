95408a30b858f56c70b1c5af5f4dbbcd0c5eb035
DanielKanyo <danielkanyog@gmail.com>
2018-02-13 12:01:59 +0100

    not needed files removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-/** Components */
-import Navigation from './Components/Navigation/Navigation';
-import Header from './Components/Header/Header';
-import About from './Components/About/About';
-import Skills from './Components/Skills/Skills';
-import Opinions from './Components/Opinions/Opinions';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Header />
-        <About />
-        <Skills />
-        <Opinions />
+      my-site-2048
       </div>
     );
   }
